91f8eebb6ba835f353730df163a43cdc6e0d1ae3
kaushalacts <kaushalkishoremnt2000@gmail.com>
2025-09-26 16:11:09 +0530

    All modules added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   region = "ap-south-1"  
-
+ }
 resource "aws_instance" "linux_servers" {
   count         = 5
-  ami           = "ami-0f58b397bc5c1f2e8" # Ubuntu 22.04 AMI ID (change per region)
-  instance_type = "t2.micro"
-  key_name      = "dev_chall"  
+  ami           = "ami-0f58b397bc5c1f2e8"  # Ubuntu / Amazon Linux
+  instance_type = "t3.micro"               # <-- Free Tier
+  key_name      = "dev_chall1"
+
   tags = {
     Name = "day31-server-${count.index + 1}"
   }
